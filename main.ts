41b2867723daedc4e2636797c766f4a194446a01
kamaii <kamaii@pop-os.localdomain>
2022-03-05 04:16:36 -0800

    restrictions

--- main.ts
+++ main.ts
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import client from "./lib/client";
 import waifu from "./slashcommands/waifu";
-import starboard from "./handlers/messages/starboard";
-client.useMessage(starboard);
+import restrictions from "./handlers/messages/restrictions";
+import addrestriction from "./slashcommands/addrestriction";
+client.useMessage(restrictions)
 client.useCommand(waifu);
+client.useCommand(addrestriction);
 client.login(process.env.TOKEN);
