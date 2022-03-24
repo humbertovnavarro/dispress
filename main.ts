e816966dae5187aac9bb9b8eb4b6507c70d9c2a0
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-03-24 01:13:48 -0700

    Added anime command

--- main.ts
+++ main.ts
@@ -6,9 +6,11 @@ import patchnotes from "./slashcommands/patchnotes";
 import musicbot from "./plugins/musicbot/plugin";
 import channeltags from "./plugins/channeltags/plugin";
 import permissions from "./plugins/permissions/plugin";
+import anime from "./slashcommands/anime";
 const main = async () => {
   client.useCommand(waifu);
   client.useCommand(patchnotes);
+  client.useCommand(anime);
   client.usePlugin(musicbot);
   client.usePlugin(channeltags);
   client.usePlugin(permissions)
