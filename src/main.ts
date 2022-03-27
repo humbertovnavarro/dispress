a83bdce3ce4b2c36b29c7cacde6c092784af50d4
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-03-27 02:58:48 -0700

    removed link reflector

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ dotenv.config();
 import client from './lib/client';
 import waifu from './slashcommands/waifu';
 import musicbot from './lib/plugins/musicbot/plugin';
-import linkreflector from './lib/plugins/linkreflector/plugin';
 import anime from './slashcommands/anime';
 const main = async () => {
   client.prefix = process.env.PREFIX || '!';
   client.useCommand(waifu);
   client.useCommand(anime);
   client.usePlugin(musicbot);
-  client.usePlugin(linkreflector);
   client.login(process.env.TOKEN);
 };
 main();
