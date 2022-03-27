6c948bc38bc3f64447c842060b65413ecb9d2593
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-03-27 01:37:35 -0700

    fixed previous commit

--- src/lib/plugins/musicbot/helpers/addLike.ts
+++ src/lib/plugins/musicbot/helpers/addLike.ts
@@ -1,6 +1,6 @@
 import type { Track } from 'discord-player';
 import type { Guild } from 'discord.js';
-import db from "../../../query/db";
+import db from '../../../query/db';
 const addLike = (track: Track, guild: Guild) => {
   try {
     db.prepare(
