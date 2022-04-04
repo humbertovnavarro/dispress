242bc29a92ee27e74fdd2a9d8dc12960f2dc349b
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-04-04 10:25:24 -0700

    lint previous commit

--- src/lib/dispress/DiscordExpressBot.ts
+++ src/lib/dispress/DiscordExpressBot.ts
@@ -1,20 +1,19 @@
-import DiscordBot from "./DiscordBot";
-import express, { Application } from "express";
-import { BotOptions } from "lib/dispress";
+import DiscordBot from './DiscordBot';
+import express, { Application } from 'express';
+import { BotOptions } from 'lib/dispress';
 export default class DiscordExpressBot extends DiscordBot {
   router = express() as Application | undefined;
   constructor(options: BotOptions) {
     super(options);
-    this.on("ready", () => {
+    this.on('ready', () => {
       const port = process.env.PORT || 3001;
       try {
         this.router?.listen(port, () => {
-          console.log(`started express server on ${port}`)
+          console.log(`started express server on ${port}`);
         });
-      }
-      catch(error) {
+      } catch (error) {
         console.error(error);
       }
-    })
+    });
   }
 }
