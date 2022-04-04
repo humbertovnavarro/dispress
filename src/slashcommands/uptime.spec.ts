c4e20243ccacc7cf60a2e43068a02bf6b94f4de8
humbertovnavarro <humberto.navarro.dev@gmail.com>
2022-04-04 14:11:15 -0700

    switched to monorepo

--- src/slashcommands/uptime.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getUptimeHumanReadable } from './uptime';
-describe('Uptime command tests', () => {
-  test('getUptimeHumanReadable works as intented', () => {
-    expect(getUptimeHumanReadable(10000)).toBe('10 seconds');
-    expect(getUptimeHumanReadable(10230120)).toBe(
-      '2 hours, 50 minutes, 30 seconds'
-    );
-  });
-});
